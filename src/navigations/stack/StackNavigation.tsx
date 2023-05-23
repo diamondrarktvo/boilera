fc37ffdbc5d7802022363a18067e0bbe80a49074
diamondrarktvo <amada.rktvo@gmail.com>
2023-05-23 20:48:02 +0300

    fix: remove android and ios source

--- src/navigations/stack/StackNavigation.tsx
+++ src/navigations/stack/StackNavigation.tsx
@@ -1,5 +1,5 @@
-import { createStackNavigator  } from "@react-navigation/stack";
-import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import { HomeScreen } from "_features";
 import { navigationName } from "_utils";
 import { stackNavigationConfig } from "./configStack";
@@ -7,17 +7,19 @@ import { stackNavigationConfig } from "./configStack";
 const Stack = createStackNavigator();
 
 const StackNavigation = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator initialRouteName={navigationName.home}>
-                <Stack.Screen name={navigationName.home} component={HomeScreen} />
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName={navigationName.home}>
+        <Stack.Screen name={navigationName.home} component={HomeScreen} />
 
-                <Stack.Group screenOptions={stackNavigationConfig.screenOptionsForDisplayedHeader}>
-                    <Stack.Screen name={navigationName.home} component={HomeScreen} />
-                </Stack.Group>
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
+        <Stack.Group
+          screenOptions={stackNavigationConfig.screenOptionsForDisplayedHeader}
+        >
+          <Stack.Screen name={navigationName.home} component={HomeScreen} />
+        </Stack.Group>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default StackNavigation;
