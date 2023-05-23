e76a803a38dd80aecbbef728eb56cfbc0369c865
diamondrarktvo <amada.rktvo@gmail.com>
2023-05-23 17:53:33 +0300

    feat: add configStack with error types

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
+import 'react-native-gesture-handler';
 import { StackNavigation } from "_navigations";
+
 
 export default function App() {
   return (
--- src/navigations/stack/StackNavigation.tsx
+++ src/navigations/stack/StackNavigation.tsx
@@ -1,15 +1,20 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator  } from "@react-navigation/stack";
 import { NavigationContainer } from '@react-navigation/native';
 import { HomeScreen } from "_features";
 import { navigationName } from "_utils";
+import { stackNavigationConfig } from "./configStack";
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 const StackNavigation = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName={navigationName.home}>
                 <Stack.Screen name={navigationName.home} component={HomeScreen} />
+
+                <Stack.Group screenOptions={stackNavigationConfig.screenOptionsForDisplayedHeader}>
+                    <Stack.Screen name={navigationName.home} component={HomeScreen} />
+                </Stack.Group>
             </Stack.Navigator>
         </NavigationContainer>
     )
