1c1428302d7edbbbfe4471d739f5d01298d6013e
diamondrarktvo <amada.rktvo@gmail.com>
2023-05-25 00:28:12 +0300

    fix: add jsx key in tsconfig

--- src/navigations/TabNavigation.tsx
+++ src/navigations/TabNavigation.tsx
@@ -1,21 +1,29 @@
 //IMPORT FROM NODE_MODULES
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
 //LOCAL IMPORT
-import { TabParamList } from './Types';
+import { TabParamList } from "./Types";
 
 //IMPORT SCREEN
-import { HomeScreen, SearchScreen } from '_features';
+import { HomeScreen, SearchScreen } from "_features";
 
 const Tab = createMaterialBottomTabNavigator<TabParamList>();
 
 const TabNavigation = () => {
-    return (
-        <Tab.Navigator>
-            <Tab.Screen name='home_screen' component={HomeScreen} options={{title: "Accueil"}} />
-            <Tab.Screen name='search_screen' component={SearchScreen} options={{title: "Recherche"}} />
-        </Tab.Navigator>
-    )
-}
+  return (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="home_screen"
+        component={HomeScreen}
+        options={{ tabBarLabel: "Accueil" }}
+      />
+      <Tab.Screen
+        name="search_screen"
+        component={SearchScreen}
+        options={{ tabBarLabel: "Recherche" }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default TabNavigation;
