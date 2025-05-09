5c9d5a8739d4f186a4dbab659791a2ad554aabc3
diamondrarktvo <amada.rktvo@gmail.com>
2025-05-09 22:08:48 +0300

    fix: add safe area view on scaffold

--- src/shared/components/Scaffold.tsx
+++ src/shared/components/Scaffold.tsx
@@ -1,45 +1,55 @@
-import Box, { BoxProps } from "./Box";
-import React from "react";
-import Text from "./Text";
+import Box, { BoxProps } from './Box';
+import React from 'react';
+import Text from './Text';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
 
 type Props = {
-  children: React.ReactNode;
-  typeOfScreen: "tab" | "stack" | "component" | "top";
-  titleTabScreen?: string;
+   children: React.ReactNode;
+   withDefaultPadding?: boolean;
+   typeOfScreen: 'tab' | 'stack' | 'component' | 'top';
+   titleTabScreen?: string;
 } & Partial<BoxProps>;
 
 type HeaderProps = {
-  title?: string | undefined;
+   title?: string | undefined;
 };
 
 const HeaderTabTitle = ({ title }: HeaderProps) => {
-  return title ? (
-    <Box paddingVertical="m" backgroundColor="mainBackground">
-      <Text variant="headerNavigation" color={"primary"}>
-        {title}
-      </Text>
-    </Box>
-  ) : null;
+   return title ? (
+      <Box paddingVertical="s" backgroundColor="mainBackground">
+         <Text variant="headerNavigation" color={'primary'}>
+            {title}
+         </Text>
+      </Box>
+   ) : null;
 };
 
 const Scaffold: React.FC<Props> = ({
-  children,
-  typeOfScreen,
-  titleTabScreen,
-  ...props
+   children,
+   typeOfScreen,
+   withDefaultPadding = true,
+   titleTabScreen,
+   ...props
 }) => {
-  return (
-    <Box
-      flex={1}
-      paddingHorizontal="s"
-      paddingVertical="s"
-      backgroundColor="mainBackground"
-      {...props}
-    >
-      {typeOfScreen === "tab" && <HeaderTabTitle title={titleTabScreen} />}
-      {children}
-    </Box>
-  );
+   return (
+      <SafeAreaView style={styles.safeAreaViewContainer}>
+         <Box
+            flex={1}
+            padding={withDefaultPadding ? 's' : 'none'}
+            backgroundColor="mainBackground"
+            {...props}>
+            {typeOfScreen === 'tab' && <HeaderTabTitle title={titleTabScreen} />}
+            {children}
+         </Box>
+      </SafeAreaView>
+   );
 };
 
 export default Scaffold;
+
+const styles = StyleSheet.create({
+   safeAreaViewContainer: {
+      flex: 1,
+   },
+});
