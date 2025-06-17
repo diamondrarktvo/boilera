eb435448a18b7b2d9642fb18c6acc0fe908deb93
diamondrarktvo <amada.rktvo@gmail.com>
2025-06-17 21:37:30 +0300

    refactore: create a listing component reusable

---
+++ src/shared/components/Listing.tsx
@@ -0,0 +1,36 @@
+import { FlashList, ListRenderItem } from '@shopify/flash-list';
+import React, { JSX } from 'react';
+import EmptyList from './EmptyList';
+import { useTranslation } from 'react-i18next';
+
+interface ListingProps<T extends { id: number | string }> {
+   data: T[];
+   renderItem: ListRenderItem<T>;
+   extraData?: T | T[] | any;
+   estimatedItemSize?: number;
+   showsVerticalScrollIndicator?: boolean;
+   emptyMessage?: string;
+}
+
+const Listing = <T extends { id: number | string }>({
+   data,
+   renderItem,
+   estimatedItemSize,
+   showsVerticalScrollIndicator,
+   emptyMessage,
+}: ListingProps<T>): JSX.Element => {
+   const { t } = useTranslation('common');
+   return (
+      <FlashList
+         keyExtractor={item => item.id.toString()}
+         estimatedItemSize={estimatedItemSize ?? 200}
+         data={data}
+         renderItem={renderItem}
+         extraData={data}
+         showsVerticalScrollIndicator={showsVerticalScrollIndicator ?? false}
+         ListEmptyComponent={<EmptyList textToShow={emptyMessage ?? t('content.empty_menu')} />}
+      />
+   );
+};
+
+export default Listing;
